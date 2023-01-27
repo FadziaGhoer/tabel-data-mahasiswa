b0947dbc2f52cca422ec280955610e52d995db50
Fadzia Ghoer Munsyori <fadziaghoermunsyori02@gmail.com>
2023-01-27 21:02:12 +0700

    Upload project web tabel data mahasiswa

---
+++ src/components/columns.js
@@ -0,0 +1,18 @@
+export const COLUMNS = [
+    {
+        Header: 'No',
+        accessor: 'id'
+    },
+    {
+        Header: 'Nama',
+        accessor: 'nama'
+    },
+    {
+        Header: 'Kelas',
+        accessor: 'kelas'
+    },
+    {
+        Header: 'IPK',
+        accessor: 'ipk'
+    }
+]
